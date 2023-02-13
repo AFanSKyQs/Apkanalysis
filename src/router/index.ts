e074bbc6ac1e716fac77f7fce2b2f6db416c3965
FanSky_Qs <3141865879@qq.com>
2023-02-13 16:17:07 +0800

    ApkAnalysis
    ApkAnalysis

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import ApkHome from '../ApkHome.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: ApkHome
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="about">
+<el-menu :default-openeds="['basic']" router>
+  <el-submenu v-for="(item,index) in $router.options.router" :key="index" :index="item.index">
+  </el-submenu>
+</el-menu>
+<h1>This is an about page</h1>
+</div>
+</template>
