fc9b9f2b8a1b3ad71ac93f1b2e6bb50939db5404
FanSky_Qs <3141865879@qq.com>
2023-02-13 20:30:39 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,16 +6,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: ApkHome
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
-]
+
+] // {
+//   path: '/about',
+//   name: 'about',
+//   // route level code-splitting
+//   // this generates a separate chunk (about.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+// }
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
