c69e8f5fecfe3f07c0ff9e3d0439b2012b8225ac
FanSky_Qs <3141865879@qq.com>
2023-02-13 20:35:29 +0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    indent: 'off',
+    'no-param-reassign': 'off',
+    'template-curly-spacing': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 600
+      }]
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ]
+}
